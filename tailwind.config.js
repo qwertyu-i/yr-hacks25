0cc57a170271592ef6d7843bf9bda35c5c3fe8f6
qwertyu <luke.z24680@gmail.com>
2025-04-08 10:34:33 -0400

    got tailwind working

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const config = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+    	'./components/**/*.{js,ts,jsx,tsx,mdx}',
+    	'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	]
+}
+
+export default config;
